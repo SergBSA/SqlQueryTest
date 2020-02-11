ab976c260bf8459d39a235bd89c281eb65790340
s.berezka <srgberezka@gmail.com>
2020-02-11 11:47:22 +0200

    Upd02.11

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace SqlQueryTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connstring = "Data Source=(gold-pen\SQLEXPRESSMES);InitialCatalog = MES_Project; Integrated Security = True";
+            string queryString = "Select * from FactSCH1_Kotly1";
+            using (SqlConnection dbconn = new SqlConnection(connstring))
+            {
+                SqlCommand command = new SqlCommand(queryString, dbconn);
+                dbconn.Open();
+                SqlDataReader reader = command.ExecuteReader();
+                try
+                {
+                    while (reader.Read())
+                    {
+                        Console.WriteLine(String.Format("{0} {1} {2} {3}", reader[0],
+                        reader[1], reader[2], reader[3]));
+                    }
+                }
+                finally
+                {
+                    reader.Close();
+                }
+            }
+        }
+    }
+}
